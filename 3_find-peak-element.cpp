26e3a6e957da2f175e72a8c1daf7985ca59cdb62
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-01 19:36:24 +0530

    first commit

---
+++ 3_find-peak-element.cpp
@@ -0,0 +1,27 @@
+https : // leetcode.com/problems/find-peak-element/
+
+        class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int pre = INT_MIN;
+        nums.push_back(INT_MIN);
+        int s = 1, e = nums.size() - 2;
+        if (e == 0)
+            return 0;
+        if (nums[1] < nums[0])
+            return 0;
+        while (s <= e)
+        {
+            int mid = (s + e) >> 1;
+            if (nums[mid] >= nums[mid - 1] && nums[mid] >= nums[mid + 1])
+                return mid;
+            else if (nums[mid] < nums[mid - 1])
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+        return -1;
+    }
+};
